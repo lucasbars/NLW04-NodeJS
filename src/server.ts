ad193094ea02f10a8483dae24896dffaf8180620
lucasbars <darklucassilva@gmail.com>
2021-05-02 17:28:08 -0300

    create models and controllers

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
+import 'reflect-metadata';
 import express, { request, response } from 'express';
+import { router } from './routes';
+import "./database";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  // return response.send("Hello World");
-  return response.json({message:"Hello World"});
-});
+app.use(express.json());
+app.use(router);
 
-app.post("/", (request, response) => {
-  return response.json({message: "dados foram salvos com sucesso"});
-});
+// app.get("/", (request, response) => {
+//   // return response.send("Hello World");
+//   return response.json({message:"Hello World"});
+// });
+
+// app.post("/", (request, response) => {
+//   return response.json({message: "dados foram salvos com sucesso"});
+// });
 
 app.listen(3333, ()=> console.log("Server is running"));
